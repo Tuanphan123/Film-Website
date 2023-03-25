d6aa2d2920b893360a0d85297c7e9880b539c495
Tuanphan123 <68193186+Tuanphan123@users.noreply.github.com>
2023-03-25 16:13:41 +0700

    Delete Film App directory

--- Film App/api/models/List.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const ListSchema = new mongoose.Schema(
-    {
-        name: {type: String, required: true, unique: true},
-        password: {type: String, required: true}
-    },
-    {timestamps: true}
-);
-module.exports = mongoose.model("List", ListSchema);
--- Film App/api/models/Movie.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const MovieSchema = new mongoose.Schema(
-    {
-        title: {type: String, required: true, unique: true}
-    },
-    {timestamps: true}
-);
-module.exports = mongoose.model("Movie", MovieSchema);
